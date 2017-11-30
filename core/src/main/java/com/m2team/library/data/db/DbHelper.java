a83e68c671a52f8d759ca0077908fab17bd81510
M2 Team <m2team.inc@gmail.com>
2017-11-30 14:40:28 +0700

    refactor code

--- core/src/main/java/com/m2team/library/data/db/DbHelper.java
+++
@@ -1,8 +0,0 @@
-package com.m2team.library.data.db;
-
-/**
- * Created by hoangminh on 8/3/2017.
- */
-
-public interface DbHelper {
-}
--- core/src/main/java/com/m2team/library/data/network/ApiHelper.java
+++
@@ -1,8 +0,0 @@
-package com.m2team.library.data.network;
-
-/**
- * Created by hoangminh on 8/2/2017.
- */
-
-public interface ApiHelper {
-}
